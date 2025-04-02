93c40de3c5ad1b2da6c2cb4d0cb0fcf774b35c98
Nitish-11k <nitishk0014@gmail.com>
2025-04-02 13:08:26 +0530

    User Dashboard and Add Contact

--- src/main/java/com/smart/controller/UserController.java
+++ src/main/java/com/smart/controller/UserController.java
@@ -1,12 +1,32 @@
 package com.smart.controller;
 
+import java.security.Principal;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.smart.dao.UserRepository;
+import com.smart.entities.Contact;
+import com.smart.entities.User;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
+
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@ModelAttribute
+	public void commonData(Model model, Principal principal){
+		String username = principal.getName();
+		User user = userRepository.getUserByUserName(username);
+		model.addAttribute("user", user);
+	}
 	
 	@RequestMapping("/dashboard")
 	public String userDashboard(Model model) {
@@ -16,7 +36,14 @@ public class UserController {
 	@RequestMapping("/profile")
 	public String userProfilePage (Model model){
 		model.addAttribute("title", "Profile");
-		return "userprofile";
+		return "/user/userprofile";
+	}
+
+	@GetMapping("/addcontact")
+	public String addContact(Model model){
+		model.addAttribute("title", "Add Contact");
+		model.addAttribute("contact", new Contact());
+		return "user/addcontact";
 	}
 
 }
